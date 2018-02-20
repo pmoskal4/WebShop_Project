f961af525d2d0de48ed9827e44beafa8c7dcb13e
pmoskal4 <pmoskal4@gmail.com>
2018-02-20 15:26:49 +0100

    Base file structure and products database

---
+++ src/js/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {HashRouter, Route, Link, Switch, NavLink} from 'react-router-dom';
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+  ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+  );
+});
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'cheap-eval-source-map',
+  entry: {
+    main: ['whatwg-fetch', path.join(__dirname, 'src/js', 'main.jsx')]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'stage-2', 'react']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/,
+        use: {loader: 'file-loader'}
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/index.html'),
+      hash: true
+    }),
+    new ExtractTextPlugin('src/js/style.css')
+  ]
+}
